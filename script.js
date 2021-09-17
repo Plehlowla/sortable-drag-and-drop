30ec8a9642f2637ab5aa25e8b2776b619302b3d3
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2021-09-17 10:03:01 -0500

    function

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 const draggables = document.querySelectorAll('.draggable')
 const containers = document.querySelectorAll('.container')
 
-draggables.forEach(draggables => {
-    draggables.addEventListener('dragstart', () => {
-        draggables.classList.add('dragging')
-    })
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
 
-    draggables.addEventListener('dragend', () => {
-        draggables.classList.remove('dragging')
-    })
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
 })
 
-containers.forEach(conatiner => {
-    containers.addEventListener('dragover', e => {
-        e.preventDefault()
-        const afterElement = getDragAfterElement(container, e.clientY)
-        const draggable = document.querySelector('.dragging')
-        if (afterElement == null) {
-            container.appendChild(draggable)
-        } else {
-            container.insertBefore(draggable, afterElement)
-        }
-    })
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
+  })
 })
 
 function getDragAfterElement(container, y) {
-    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
 
-    return draggableElements.reduce((closest, child) => {
-        const box = child.getBoundingClientRect()
-        const offset = y - box.top - box.height / 2
-        if (offset < 0 && offset > closest.offset) {
-            return { offset: offset, element: child}
-        } else {
-            return closest
-        }
-    }, {offset: Number.NEGATIVE_INFINITY}).element
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
 }
